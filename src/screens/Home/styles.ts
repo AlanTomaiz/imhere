8fc0b15b96abf7eeb135e06baaaf63c91c3980b4
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2024-12-24 03:21:01 -0300

    feat: add button

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -6,7 +6,6 @@ export const styles = StyleSheet.create({
     backgroundColor: '#131016',
     padding: 24
   },
-
   eventName: {
     color: '#FDFCFE',
     fontSize: 24,
@@ -16,5 +15,32 @@ export const styles = StyleSheet.create({
   eventDate: {
     color: '#6B6B6B',
     fontSize: 16
+  },
+  input: {
+    flex: 1,
+    height: 56,
+    padding: 16,
+    backgroundColor: '#1F1F1F',
+    color: '#FDFCFE',
+    borderRadius: 5
+  },
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+    gap: 12,
+    marginTop: 36,
+    marginBottom: 42,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31CF67',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: '#FDFCFE',
+    fontSize: 24
   }
 })
